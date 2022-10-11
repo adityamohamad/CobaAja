445eacdb18eb98b352f297d32bf20406f3c3b991
adityamohamad <48940215+adityamohamad@users.noreply.github.com>
2022-10-11 14:47:04 +0700

    menambah baris baru

--- ApaKabar.java
+++ ApaKabar.java
@@ -1,5 +1,7 @@
 class ApaKabar {
     public static void main(String[] args) {
         System.out.println("Apa Kabar!"); 
+        System.out.println("Bagaimana keadaan disana!"); 
+        System.out.println("Nama anda siapa!"); 
     }
 }
